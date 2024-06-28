af87066ae01dafacf41e71cae689d977d58400cd
Sinthoras39 <digc0820@gmail.com>
2024-06-29 01:13:42 +0200

    init

---
+++ home-manager/home.nix
@@ -0,0 +1,138 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "dgc";
+  home.homeDirectory = "/home/dgc";
+
+  home.stateVersion = "23.11"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs; [
+    alacritty
+    brave
+    # more stuff here
+
+    # required packages
+    dconf
+  ];
+
+  programs.git = {
+    enable = true;
+    userName = "Sinthoras39";
+    userEmail = "digc0820@gmail.com";
+  };
+
+  programs.zsh = {
+    autocd = true;
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    shellAliases = {
+      la = "ls -la";
+      sys-update = "sudo nixos-rebuild switch";
+      update = "home-manager switch";
+      ed = "emacsclient -a 'emacs'";
+      emacs = "emacsclient -ca 'emacs'";
+      sued = "sudoedit";
+      clean = "nix-collect-garbage -d";
+    };
+    history = {
+      size = 1000;
+      path = "${config.xdg.dataHome}/zsh/history";
+    };
+    plugins = with pkgs; [
+      {
+        name = "agkozak-zsh-prompt";
+        src = fetchFromGitHub {
+          owner = "agkozak";
+          repo = "agkozak-zsh-prompt";
+          rev = "v3.7.0";
+          sha256 = "1iz4l8777i52gfynzpf6yybrmics8g4i3f1xs3rqsr40bb89igrs";
+        };
+        file = "agkozak-zsh-prompt.plugin.zsh";
+      }
+      {
+        name = "formarks";
+        src = fetchFromGitHub {
+          owner = "wfxr";
+          repo = "formarks";
+          rev = "8abce138218a8e6acd3c8ad2dd52550198625944";
+          sha256 = "1wr4ypv2b6a2w9qsia29mb36xf98zjzhp3bq4ix6r3cmra3xij90";
+        };
+        file = "formarks.plugin.zsh";
+      }
+      {
+        name = "zsh-syntax-highlighting";
+        src = fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-syntax-highlighting";
+          rev = "0.6.0";
+          sha256 = "0zmq66dzasmr5pwribyh4kbkk23jxbpdw4rjxx0i7dx8jjp2lzl4";
+        };
+        file = "zsh-syntax-highlighting.zsh";
+      }
+      {
+        name = "zsh-abbrev-alias";
+        src = fetchFromGitHub {
+          owner = "momo-lab";
+          repo = "zsh-abbrev-alias";
+          rev = "637f0b2dda6d392bf710190ee472a48a20766c07";
+          sha256 = "16saanmwpp634yc8jfdxig0ivm1gvcgpif937gbdxf0csc6vh47k";
+        };
+        file = "abbrev-alias.plugin.zsh";
+      }
+      {
+        name = "zsh-autopair";
+        src = fetchFromGitHub {
+          owner = "hlissner";
+          repo = "zsh-autopair";
+          rev = "34a8bca0c18fcf3ab1561caef9790abffc1d3d49";
+          sha256 = "1h0vm2dgrmb8i2pvsgis3lshc5b0ad846836m62y8h3rdb3zmpy1";
+        };
+        file = "autopair.zsh";
+      }
+    ];
+  };
+
+  qt.enable = true;
+  qt.style.name = "adwaita-dark";
+
+  gtk = {
+    enable = true;
+    theme.name = "adwaita-dark";
+    cursorTheme.name = "Bibata-Modern-Ice";
+    iconTheme.name = "GruvboxPlus";
+  };
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/dgc/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    EDITOR = "emacsclient -a 'emacs'";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
